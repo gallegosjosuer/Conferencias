c6ee06362b6001f3026cb588443b1ff53b36b663
Josue <gallegosjosuer@gmail.com>
2024-03-06 16:09:33 -0600

    Login y registro funcional

---
+++ client/src/app/components/conferencia/conferencia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conferencia',
+  standalone: true,
+  imports: [],
+  templateUrl: './conferencia.component.html',
+  styleUrl: './conferencia.component.css'
+})
+export class ConferenciaComponent {
+
+}
---
+++ client/src/app/components/login/login.component.ts
@@ -0,0 +1,101 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { FormControl, FormsModule } from '@angular/forms';
+import { LoginService } from '../../services/login.service';
+import { User } from '../../models/user.model';
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatButtonModule,
+    HttpClientModule,
+  ],
+  providers: [LoginService],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  tabIndex = 0;
+  remember_session = false;
+  public userLogIn: User;
+  public userSignUp: User;
+
+  constructor(private router: Router, private loginService: LoginService) {
+    this.userLogIn = new User();
+    this.userSignUp = new User();
+  }
+
+  login() {
+    this.router.navigate(['/conferencia']);
+  }
+
+  validateLogin() {
+    if (this.userLogIn.username && this.userLogIn.password) {
+      this.loginService.validateLogin(this.userLogIn).subscribe(
+        (result) => {
+          if (result) {
+            this.login();
+          } else {
+            alert('Contraseña equivocada');
+          }
+          console.log(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    } else {
+      alert('Ingresar un usuario y contraseña');
+    }
+  }
+
+  signUp() {
+    if (this.userSignUp.username && this.userSignUp.password) {
+      this.loginService.validateSignUp(this.userSignUp).subscribe(
+        (userAlreadyExists) => {
+          if (userAlreadyExists) {
+            alert('El usuario ya existe');
+          } else {
+            this.loginService.signUp(this.userSignUp).subscribe(
+              (result) => {
+                this.login();
+                console.log(result);
+              },
+              (error) => {
+                console.log(error);
+              }
+            );
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    } else {
+      alert('Ingresar un usuario y contraseña');
+    }
+  }
+
+  keepSession() {
+    // TODO
+    console.log('RECORDAR SESION:', this.remember_session);
+  }
+
+  changeTab() {
+    this.tabIndex = (this.tabIndex + 1) % 2;
+  }
+}
